4ae207aa1c4559602c18cdaba1726673ed914a62
iamnderitum <iamnderitum@gmail.com>
2023-08-04 17:08:09 +0300

    GraphQL schema - Definining the schema Object Types and Query. Django Migrations

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,21 @@
 import graphene
+from graphene_django import DjangoObjectType
+from core import models
+from graphQLapi import models
+
+class User(DjangoObjectType):
+    class Meta:
+        model = models.User
+
+class CartItem(DjangoObjectType):
+    class Meta:
+        model = models.CartItem
+
+class Cart(DjangoObjectType):
+    class Meta:
+        model = models.Cart
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value = "Hi !")
+    user = graphene.Field(User, id=graphene.Int())
 
 schema = graphene.Schema(query=Query)
--- graphQLapi/models.py
+++ graphQLapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+""""id": <<INT>>,
+"itemNo": <<INT>>,
+"quantity": <<INT>>,
+"amount": <<INT>>
+ "itemName": <<STRING>>"""
+
+class CartItem(models.Model):
+    itemName = models.CharField(max_length=100, blank=True, null=True)
+    itemNo = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    amount = models.IntegerField(null=True, blank=True)
+
+class Cart(models.Model):
+    cartItems = models.ForeignKey("graphQLapi.CartItem", on_delete=models.CASCADE)
+    user = models.ForeignKey('core.User', on_delete=models.CASCADE)
+
+
+
